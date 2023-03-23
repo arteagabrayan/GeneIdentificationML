58ff21585e597f97b4d2a92f6b123fe8ca778744
arteagabrayan <arteagabrayan99ah@gmail.com>
2023-03-23 16:48:51 -0500

    upt

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.title("Aplicaciones de machine learning sobre heterogeneidad intratumoral en glioblastoma utilizando datos de secuenciación de ARN")
 st.subheader("Industrias 4.0 - Una visión práctica")
 
+print("Working...")
